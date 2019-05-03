a0dd1b0a477e3fefd8fd5c340ffbc59bea7db3bf
Jenny Dang <qanhdang96@hotmail.com>
2019-05-03 14:06:44 -0500

    new homework

--- 12-Web-Scraping-and-Document-Databases/Instructions/scrape_mars.py
+++ 12-Web-Scraping-and-Document-Databases/Instructions/scrape_mars.py
@@ -20,7 +20,7 @@ def scrape():
         html = browser.html
         soup = bs(html, 'html.parser')
 
-        news_title = soup.find('div', class_='content_title').find('a').text
+        news_title = soup.find('div', class_='content_title').text
         news_p = soup.find('div', class_='article_teaser_body').text
         mars_info['news_title']= news_title
         mars_info['news_p']= news_p
@@ -40,7 +40,7 @@ def image():
         html = browser.html
         soup = bs(html, 'html.parser')
 
-        image = soup.find("img", class_="thumb")["src"]
+        image = soup.find('article')['style'].replace('background-image: url(','').replace(');', '')[1:-1]
         featured_image_url = "https://www.jpl.nasa.gov" + image
         mars_info['featured_image_url'] = featured_image_url
 
@@ -103,8 +103,7 @@ def hemisphere():
             browser.visit(main_url + item_url)
             item_html = browser.html 
             soup = bs(item_html, 'html.parser')
-            img_url = soup.find('div', class_='downloads').find("a")["href"]
-
+            img_url = main_url + soup.find('img', class_='wide-image')['src']
             hemisphere_image_urls.append({"title" : title, "img_url" : img_url})
             mars_info['hemisphere_image_urls'] = hemisphere_image_urls
         return mars_info
